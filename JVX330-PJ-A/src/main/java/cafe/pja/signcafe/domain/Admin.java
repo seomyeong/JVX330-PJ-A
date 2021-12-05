4a5cfc3958d07140e90578116e9c185421da54d1
seomyeong <arri123@naver.com>
2021-12-05 18:54:27 +0900

    test서명

--- JVX330-PJ-A/src/main/java/cafe/pja/signcafe/domain/Admin.java
+++
@@ -1,7 +0,0 @@
-package cafe.pja.signcafe.domain;
-
-public class Admin {
-	private long orderNum; //주문수량
-	private double eachTotalPrice; //메뉴별 매출액
-	private double totalPrice; //카페총매출액
-}
--- JVX330-PJ-A/src/main/java/cafe/pja/signcafe/service/AdminService.java
+++ JVX330-PJ-A/src/main/java/cafe/pja/signcafe/service/AdminService.java
@@ -1,5 +1,14 @@
 package cafe.pja.signcafe.service;
 
 public interface AdminService {
+	//전체회원조회
+	
 
+	//총 판매 매출 조회
+	
+	
+	//메뉴별 매출액 & 수량 조회
+	
+	
+	//카테고리별 총매출액 조회
 }
