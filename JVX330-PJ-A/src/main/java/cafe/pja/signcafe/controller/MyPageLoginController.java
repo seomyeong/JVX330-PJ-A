9f5f97a6650d67daba0c86f7f5b7a60a9a37fbca
jyoungch <81287664+jyoungch@users.noreply.github.com>
2021-12-07 15:20:28 +0900

    지영 update

--- JVX330-PJ-A/src/main/java/cafe/pja/signcafe/controller/MyPageLoginController.java
+++ JVX330-PJ-A/src/main/java/cafe/pja/signcafe/controller/MyPageLoginController.java
@@ -2,9 +2,13 @@ package cafe.pja.signcafe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/myPageService/myPageLogin")
 public class MyPageLoginController {
-	
+	@RequestMapping(method = RequestMethod.GET)
+	public String myPageLogin() {
+		return "myPageService/myPageLogin";
+	}
 }
