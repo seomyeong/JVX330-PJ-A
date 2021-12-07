7d3d61bdc291907c0d3a565a8b3b79ac01dd223b
kim-jong-seong <ydh1178@naver.com>
2021-12-07 20:50:17 +0900

    종성 - addUserController.java, UserCommand.java

---
+++ JVX330-PJ-A/src/main/java/cafe/pja/signcafe/controller/UserCommand.java
@@ -0,0 +1,27 @@
+package cafe.pja.signcafe.controller;
+
+public class UserCommand {
+	private String userName;
+	private String userPhone;
+	private String userPw;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+}
