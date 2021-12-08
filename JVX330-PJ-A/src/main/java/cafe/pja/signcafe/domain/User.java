64357194c8b52b30c1c23f4cedac7268f03e7d57
kim-jong-seong <ydh1178@naver.com>
2021-12-08 14:40:41 +0900

    종성 - AddUser부분 sql빼고 완

--- JVX330-PJ-A/src/main/java/cafe/pja/signcafe/domain/User.java
+++ JVX330-PJ-A/src/main/java/cafe/pja/signcafe/domain/User.java
@@ -10,5 +10,51 @@ public class User {
 	private double mileage;
 	private Date regDate;
 	
+	public User() {}
+	
+	public User(String name, String phone, String passWd) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.passWd = passWd;
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPassWd() {
+		return passWd;
+	}
+	public void setPassWd(String passWd) {
+		this.passWd = passWd;
+	}
+	public double getMileage() {
+		return mileage;
+	}
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
 	
 }
