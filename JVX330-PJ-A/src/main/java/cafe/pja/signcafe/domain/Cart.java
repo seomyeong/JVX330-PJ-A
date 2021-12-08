2b2420ddcaf5aedf9e104e0041fdcb3871e5577a
seomyeong <arri123@naver.com>
2021-12-08 10:52:52 +0900

    SQL UPDATE

--- JVX330-PJ-A/src/main/java/cafe/pja/signcafe/domain/Cart.java
+++ JVX330-PJ-A/src/main/java/cafe/pja/signcafe/domain/Cart.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class Cart {
 	private long cartNum; // PK
+	private CartId cartId;
 	private String cart_menuName;
 	private double cart_menuPrice;
 	private long cart_menuCount;
 	private double cart_totalPrice;
 	private Extra extra;
-	private CartId cartId;
 	
 }
