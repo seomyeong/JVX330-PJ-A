4f448094669acc7012bb6f7960d9c518fcab85db
seomyeong <arri123@naver.com>
2021-12-14 16:48:01 +0900

    서명 OrderedList

--- JVX330-PJ-A/src/main/java/cafe/pja/signcafe/dao/OrderedListDao.java
+++ JVX330-PJ-A/src/main/java/cafe/pja/signcafe/dao/OrderedListDao.java
@@ -1,5 +1,21 @@
 package cafe.pja.signcafe.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import cafe.pja.signcafe.domain.OrderedList;
+
 public class OrderedListDao {
+	private JdbcTemplate jdbcTemplate;
+	
+	public OrderedListDao(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
+	
+	//MenuInfo테이블에 주문갯수 update
+	public void updateMenuCount(OrderedList ordered) {
+		String sql = "UPDATE INTO MENU_INFO SET MENUCOUNT=? WHERE ordered_MenuNum=?";
+		jdbcTemplate.update(sql, (Long)ordered.getMenuInfo().getMenuCount() + ordered.getMenuCount(), ordered.getMenuInfo().getMenuNum());
+	}
 	
 }
--- JVX330-PJ-A/src/main/webapp/WEB-INF/views/menuService/menu_Page.jsp
+++ JVX330-PJ-A/src/main/webapp/WEB-INF/views/menuService/menu_Page.jsp
@@ -3,6 +3,8 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 
+
+
 <!DOCTYPE html>
 <html>
 <head>
